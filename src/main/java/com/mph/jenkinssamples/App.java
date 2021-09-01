f414ac306209ccd7426b00cde276f8cfb1490b6b
chiranjeevi vangala <chiranjeevi3212@gmail.com>
2021-09-01 14:17:22 +0530

    initial commit

---
+++ src/main/java/com/mph/jenkinssamples/App.java
@@ -0,0 +1,13 @@
+package com.mph.jenkinssamples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
